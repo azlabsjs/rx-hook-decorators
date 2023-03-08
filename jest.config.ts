b861278f2e375f54d6e786196c9e8a7b2024bbc1
azandrew-sidoine <asmyns.platonnas29@gmail.com>
2023-03-08 07:53:13 +0000

    updated package.json

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,7 @@ import type { JestConfigWithTsJest } from 'ts-jest'
 
 const jestConfig: JestConfigWithTsJest = {
   // [...]
+  preset: 'ts-jest/presets/default-esm',
   extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
